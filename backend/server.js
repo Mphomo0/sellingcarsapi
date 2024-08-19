3a476585b4458a6b626b3df704a2851bcb4e2240
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2024-08-19 14:42:27 +0200

    added cors origin

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({}))
+app.use(
+  cors({
+    origin: 'https://sellingcars.onrender.com',
+    credentials: include,
+  })
+)
 
 // Handle image uploads
 app.post('/api/upload', uploadArray, (req, res) => {
